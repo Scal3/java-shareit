e54afecae7325f5b0c0a7f598fda2911635fde34
Herman <cool.goga501@gmail.com>
2024-02-20 23:23:18 +0100

    feat/create ItemController

---
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -0,0 +1,69 @@
+package ru.practicum.shareit.item.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.service.ItemService;
+
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@RestController
+@Validated
+@RequestMapping("/items")
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ItemDto createItem(
+            @RequestHeader("X-Sharer-User-Id") long userId,
+            @RequestBody ItemDto dto
+    ) {
+        return itemService.createItem(userId, dto);
+    }
+
+    @PatchMapping(
+            value = "/{itemId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ItemDto updateItem(
+            @RequestHeader("X-Sharer-User-Id") long userId,
+            @PathVariable @Positive long itemId,
+            @RequestBody ItemDto dto
+    ) {
+        dto.setId(itemId);
+
+        return itemService.updateItem(userId, dto);
+    }
+
+    @GetMapping(
+            value = "/{itemId}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ItemDto getOneItemById(@PathVariable @Positive long itemId) {
+        return itemService.getOneItemById(itemId);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<ItemDto> getOwnersItems(@RequestHeader("X-Sharer-User-Id") long userId) {
+        return itemService.getOwnersItems(userId);
+    }
+
+    @GetMapping(
+            value = "/search",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<ItemDto> getAvailableItemsBySearchString(@RequestParam String text) {
+        return itemService.getAvailableItemsBySearchString(text);
+    }
+}
