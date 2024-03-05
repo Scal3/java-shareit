cf713c554764a673d460449771d02690f36fac34
Herman <cool.goga501@gmail.com>
2024-03-05 13:14:11 +0100

    feat/add JPA annotations to User and Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,26 +1,33 @@
 package ru.practicum.shareit.item.model;
 
 import lombok.Data;
-import ru.practicum.shareit.review.model.Review;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.List;
+import javax.persistence.*;
 
-/**
- * TODO Sprint add-controllers.
- */
+@Entity
+@Table(name = "items")
 @Data
+@NoArgsConstructor
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String description;
 
+    @Column(nullable = false)
     private boolean available;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User owner;
 
-    private List<Review> reviews;
+//    private List<Review> reviews;
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,25 +1,30 @@
 package ru.practicum.shareit.user.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.review.model.Review;
 
+import javax.persistence.*;
 import java.util.List;
 
-/**
- * TODO Sprint add-controllers.
- */
-
+@Entity
+@Table(name = "users")
 @Data
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String name;
 
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
     private List<Item> items;
 
-    private List<Review> reviews;
+//    private List<Review> reviews;
 }
